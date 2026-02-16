2ada0ffcd7e042babd449b1400af3ec641f3d8c1
DMYC <dreammakersysc@gmail.com>
2026-02-16 21:47:59 +0530

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,49 @@
+const CACHE_NAME = 'dmysc-v1';
+const ASSETS_TO_CACHE = [
+    './',
+    './index.html',
+    './assets/css/home-style.css',
+    './js/supabase.js',
+    './assets/images/logo_1.png',
+    './assets/images/logo_2.png',
+    'https://cdn.tailwindcss.com',
+    'https://unpkg.com/aos@2.3.1/dist/aos.css',
+    'https://unpkg.com/aos@2.3.1/dist/aos.js'
+];
+
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then((cache) => {
+                console.log('[Service Worker] Caching all: app shell and content');
+                return cache.addAll(ASSETS_TO_CACHE);
+            })
+    );
+});
+
+self.addEventListener('fetch', (event) => {
+    event.respondWith(
+        caches.match(event.request)
+            .then((response) => {
+                // Cache hit - return response
+                if (response) {
+                    return response;
+                }
+                return fetch(event.request);
+            })
+    );
+});
+
+self.addEventListener('activate', (event) => {
+    event.waitUntil(
+        caches.keys().then((cacheNames) => {
+            return Promise.all(
+                cacheNames.map((cacheName) => {
+                    if (cacheName !== CACHE_NAME) {
+                        return caches.delete(cacheName);
+                    }
+                })
+            );
+        })
+    );
+});
